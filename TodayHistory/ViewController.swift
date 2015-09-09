f73818ca23a18b4bc50dbaf301820fb26f39b3fd
谭伟 <tanwei.rush@gmail.com>
2015-09-09 16:10:32 +0800

    Initial Commit

---
+++ TodayHistory/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TodayHistory
+//
+//  Created by 谭伟 on 15/9/9.
+//  Copyright (c) 2015年 谭伟. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
